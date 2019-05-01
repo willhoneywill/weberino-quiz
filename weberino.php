b93bb7ec84148251c13f79e993bd7369a707ecb5
Will Honeywill <willhoneywill@gmail.com>
2019-05-01 11:34:41 +0200

    first commit

---
+++ weberino.php
@@ -0,0 +1,7 @@
+/*
+Plugin Name: Weberino Quiz Creator
+Plugin URI: https://www.weberino.com/
+Description: Easily create a viral quiz for your website with this great quiz creator.
+Version: 1.0.0
+Author: Weberino
+*/
