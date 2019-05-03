1134e2f5009ba721ef0eed7fc355cebe17d9aa22
Will Honeywill <whoneywill@adminisatorsmbp.helios.local>
2019-05-03 12:01:14 +0200

    created class

--- weberino.php
+++ weberino.php
@@ -6,7 +6,6 @@ Description: Easily create a viral quiz for your website with this great quiz cr
 Version: 1.0.0
 Author: Weberino
 */
-
 /*
 create custom post
 create custom fields
@@ -20,62 +19,74 @@ shortcode
 social share
 */
 
-function create_post_type() {
-  register_post_type( 'quiz',
-    array(
-      'labels' => array(
-        'name' => __( 'Quizzes' ),
-        'singular_name' => __( 'Quiz' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'show_in_menu' => true
-    )
-  );
-}
-add_action( 'init', 'create_post_type' );
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-function weberino_add_questions_html($post)
-{
-    require_once plugin_dir_path( __FILE__ ) . 'templates/quiz-table.php';
-}
+class Weberino {
 
-function weberino_add_questions()
-{
-    add_meta_box(
-            'weberino_question',
-            'Add hints or questions',
-            'weberino_add_questions_html',
-            'quiz'
-        );
-}
-
-add_action('add_meta_boxes', 'weberino_add_questions');
-
-function weberino_save_postdata($post_id)
-{
-	$x = 1;
-
-	while ($x <= 10) {
-	    if (array_key_exists('weberino_question_'. $x, $_POST)) {
-		    $question = sanitize_text_field($_POST['weberino_question_'. $x]);
-	        update_post_meta(
-	            $post_id,
-	            'weberino_question_'. $x,
-	            $question
-	        );
-	    }
-
-	    if (array_key_exists('weberino_answer_'. $x, $_POST)) {
-		    $answer = sanitize_text_field($_POST['weberino_answer_'. $x]);
-	        update_post_meta(
-	            $post_id,
-	            'weberino_answer_'. $x,
-	            $answer
-	        );
-	    }
-	    $x++;
+	function  __construct() {
+		$this->register_actions;
 	}
-    
+
+	function register_actions() {
+		add_action( 'init', [$this, 'create_post_type'] );
+		add_action('add_meta_boxes', [$this, 'weberino_add_questions']);
+		add_action('save_post', [$this, 'weberino_save_postdata']);
+		//enqueue js and css
+	}
+
+	function create_post_type() {
+		register_post_type( 'quiz',
+			array(
+				'labels' => array(
+					'name' => __( 'Quizzes' ),
+					'singular_name' => __( 'Quiz' )
+				),
+				'public' => true,
+				'has_archive' => true,
+				'show_in_menu' => true
+			)
+		);
+	}
+
+	function weberino_add_questions() {
+		add_meta_box(
+			'weberino_question',
+			'Add hints or questions',
+			[$this, 'weberino_add_questions_html'],
+			'quiz'
+		);
+	}
+
+	function weberino_add_questions_html($post) {
+		require_once plugin_dir_path( __FILE__ ) . 'templates/quiz-table.php';
+	}
+
+	function weberino_save_postdata($post_id) {
+		$x = 1;
+
+		while ($x <= 10) {
+			if (array_key_exists('weberino_question_'. $x, $_POST)) {
+				$question = sanitize_text_field($_POST['weberino_question_'. $x]);
+				update_post_meta(
+					$post_id,
+					'weberino_question_'. $x,
+					$question
+				);
+			}
+
+			if (array_key_exists('weberino_answer_'. $x, $_POST)) {
+				$answer = sanitize_text_field($_POST['weberino_answer_'. $x]);
+				update_post_meta(
+					$post_id,
+					'weberino_answer_'. $x,
+					$answer
+				);
+			}
+			$x++;
+		}
+
+	}
 }
-add_action('save_post', 'weberino_save_postdata');
+
+$weberino = new Weberino();
+$weberino->register_actions();
